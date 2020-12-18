f5005e9445bb743ecec0da9a368c7b5cd2af1fbd
Angie Xu <angelinaxuwenlan@gmail.com>
2020-12-18 20:58:12 +1100

    my hello world

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b) {
+	return a+b;
+}
+
+int main() {
+	int a = 10;
+	int b = 20;
+	int c = add(a, b);
+	cout << c << endl;
+	cout << "hello world" <<endl;
+}
