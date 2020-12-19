fdb69bb2e2d24650e47665557f3ffc6b00718a32
AngieXuAU <angelinaxuwenlan@gmail.com>
2020-12-20 08:28:27 +1100

    previous exercises

---
+++ Chapter_2/right_alignment.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin>>a>>b>>c;
+	printf("%8d\n",a);
+	printf("%8d\n",b);
+	printf("%8d",c);
+	return 0;
+}
---
+++ Chapter_2/right_alignment_2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin>>a>>b>>c;
+	printf("%8d\n%8d\n%8d",a,b,c);
+	return 0;
+}
