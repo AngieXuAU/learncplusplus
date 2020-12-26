e81744cea07defff83b5f392c2839618b94e1627
AngieXuAU <angelinaxuwenlan@gmail.com>
2020-12-27 08:53:58 +1100

    Chapter 3 Loop Exercises cont. 27.12

--- Chapter_3/printing_a_square.cpp
+++ Chapter_3/printing_a_square.cpp
@@ -2,8 +2,20 @@
 #include<cstdio>
 using namespace std;
 int main(){
-	int Rows, Columns;
-	cin>>Rows>>Columns;
-	char Ch=getchar();
-	char Pa=getchar();
+	int Rows, Columns, Parity;
+	char Ch;
+	scanf("%d%d %c %d", &Rows, &Columns, &Ch, &Parity);
+//	cout<<Rows<<Columns<<Ch<<Parity;
+	for(int i=1; i<=Rows; i++){
+		for(int in=1; in<=Columns; in++){
+//			if(Parity ==1 || i==1 || i==Rows || in==1 || in==Columns){
+//				cout<<Ch;
+//			} else {
+//				cout<<" ";
+//			}
+			printf("%c", (Parity ==1 || i==1 || i==Rows || in==1 || in==Columns? Ch:' '));
+			//cout<<(Parity ==1 || i==1 || i==Rows || in==1 || in==Columns? Ch:' ');
+		}
+		cout<<endl;
+	}
 }
---
+++ Chapter_3/printing_out_a_square.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+	int Length, Width, Parity;
+	char Ch;
+	scanf("%d%d %c %d", &Length, &Width, &Ch, &Parity);
+//	cout<<Length<<Width<<Ch<<endl;
+	if(){
+		for(int i=1; i<=Length; i++){
+			for(int in=1; in<=Width; in++){
+				cout<<Ch;
+			}
+			cout<<endl;
+		}
+//	} else {
+//		cout<<Ch;
+//		cout<<endl;
+//		for(int i=1; i<=Lenght; i++){
+//			cout<<" ";
+//			for()
+//		}
+//	}
+}
