eb1260034006685d42c961c83493e042cc99e3b2
AngieXuAU <angelinaxuwenlan@gmail.com>
2020-12-26 21:53:53 +1100

    Updates

--- Chapter_2/character_input_output.cpp
+++ Chapter_2/character_input_output.cpp
@@ -3,13 +3,7 @@
 using namespace std;
 int main()
 {
-//	char ch;
-//	ch=getchar();
-//	cout<<ch*1;
-//	char ch;
-//	ch=getchar();
-//	char cha;
-//	cha=getchar();
-//	cout<<ch<<" "<<cha;
-	cout<<"1+2";
+	char ch=getchar();
+	char cha=getchar();
+	cout<<ch<<endl<<cha;
 }
---
+++ Chapter_3/printing_a_square.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+	int Rows, Columns;
+	cin>>Rows>>Columns;
+	char Ch=getchar();
+	char Pa=getchar();
+}
