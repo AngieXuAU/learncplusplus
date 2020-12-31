787a104169f25d9c3cbf48786d4ef5b0bc77f7aa
AngieXuAU <angelinaxuwenlan@gmail.com>
2020-12-31 15:44:08 +1100

    Updates

---
+++ finding_average_random_questions.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(){
+	int i, j, number;
+	cout<<"Find the average of these sets of numbers: "<<endl;
+	for(i=1; i<=10; i++){
+		int maxn=i+4;
+		for(j=5; j<=maxn; j++){
+			number=(rand() % 40+10);
+			cout<<number;
+			if(j<maxn){
+				cout<<", ";
+			} else if (j==maxn){
+				cout<<": "<<endl<<endl<<endl;
+				continue;
+			}
+		}
+	}
+}
