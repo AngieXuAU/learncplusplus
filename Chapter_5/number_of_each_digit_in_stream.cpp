ec35f9d1c2a719e0b997348e59c1b6effceef46f
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-01-09 10:32:52 +1100

    Number of each digits in stream 9.01.21

---
+++ Chapter_5/number_of_each_digit_in_stream.cpp
@@ -0,0 +1,53 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+using namespace std;
+int main(){
+	int Number[10]={0};
+	char Input[255];
+	gets(Input);
+	
+	for(int i=0; i<=strlen(Input); ++i){
+		if(int(Input[i])>=48 && int(Input[i])<=57){
+			Number[int(Input[i])-48] += 1;
+		}
+		
+		
+//		switch(Input[i]){
+//			case '0':
+//				Number[0]+=1;
+//				break;
+//			case '1':
+//				Number[1]+=1;
+//				break;
+//			case '2':
+//				Number[2]+=1;
+//				break;
+//			case '3':
+//				Number[3]+=1;
+//				break;
+//			case '4':
+//				Number[4]+=1;
+//				break;
+//			case '5':
+//				Number[5]+=1;
+//				break;
+//			case '6':
+//				Number[6]+=1;
+//				break;
+//			case '7':
+//				Number[7]+=1;
+//				break;
+//			case '8':
+//				Number[8]+=1;
+//				break;
+//			case '9':
+//				Number[9]+=1;
+//				break;
+//		}
+	}
+	
+	for(int j=0; j<10; j++){
+		cout<<j<<"\t"<<Number[j]<<endl;
+	}
+}
