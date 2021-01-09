ee5f973d885bc13a79a7333c94055afd4071e22a
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-01-09 11:24:27 +1100

    Finding the first only letter 09.01.21

---
+++ Chapter_5/first_only_letter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+	int Letter[26];
+	char Input[100000];
+	gets(Input);
+	
+	for(int i=0; i<=strlen(Input); ++i){
+		Letter[int(Input[i])-97]+=1;
+	}
+	
+	for(int i=0; i<=26; ++i){
+		if(Letter[i]==1){
+			cout<<char(i+97);
+		}
+	}
+}
