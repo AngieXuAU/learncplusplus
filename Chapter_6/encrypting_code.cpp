d623d0bbe2e640c591652b91aec58b28152c55c0
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-01-20 14:22:31 +1100

    Functions update2 20.01.21

--- Chapter_6/encrypting_code.cpp
+++ Chapter_6/encrypting_code.cpp
@@ -1,4 +1,21 @@
-//
-// Created by angel on 20/01/2021.
-//
+#include <cstring>
+#include <iostream>
+using namespace std;
+char Decode(char r, char k){
+    char c;
+    int i;
+    c=int(r)-(int(k)-97);
+    return c;
+}
+int main(){
+    char Key[100], Result[100], Message[100];
+    gets(Key);
+    gets(Result);
 
+    for (int i = 0; i < strlen(Result); ++i) {
+        Message[i]=Decode(Result[i], Key[i]);
+    }
+
+    puts(Message);
+    return 0;
+}
