9afffcbd93c0e485319ae1f73c6b681165122910
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-01-20 22:12:21 +1100

    Functions update3 20.01.21

---
+++ Chapter_6/find_input_in_paragraph.cpp
@@ -0,0 +1,34 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+void Find(char w[10], char t[200]){
+    int j, k, First, Counter;
+    j=-1;
+    k=0;
+    Counter=0;
+    for (int i = 0; i < strlen(t); ++i) {
+        while (t[i]!=' '){
+            if (w[++k]==t[i]){
+                if (j=-1) {
+                    First = i;
+                    j = 0;
+                }
+            }
+        }
+        if (t[i]==' '){
+            k=0;
+            Counter++;
+        }
+    }
+    cout<<Counter<<"\t"<<First;
+}
+int main(){
+    char Word[10], Text[200];
+    gets(Word);
+    strlwr(Word);
+    gets(Text);
+    strlwr(Text);
+
+    Find(Word, Text);
+    return 0;
+}
--- Chapter_6/finding_word_in_paragraph.cpp
+++ Chapter_6/finding_word_in_paragraph.cpp
@@ -1,12 +1,32 @@
 #include <cstring>
 #include <iostream>
 using namespace std;
-bool Find(char W[], char T[][6]){
+bool Find(char w, char t){
+    while (t!=' '){
+        for (int i = 0 ; i < strlen(w); ++i) {
 
+        }
+    }
 }
 int main(){
-    char W[10], Text[20][10];
-    gets(W);
-    strlwr(W);
-    do
+    char Word[10], Text[200];
+    int j=-1, First, Counter;
+    gets(Word);
+    strlwr(Word);
+    gets(Text);
+    strlwr(Text);
+
+    for (int i = 0; i < strlen(Text); ++i) {
+        if (Text[i]!=' '){
+            if (Find(Word[0], Text[i])){
+                Counter++;
+                if (j=-1) {
+                    First = i;
+                    j = 0;
+                }
+            }
+        }
+    }
+
+    cout<<Counter<<"\t"<<First;
 }
