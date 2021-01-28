84d0fb912c8f528140065245ad5832730cf3a590
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-01-28 16:57:09 +1100

    Functions recursive pre-exercises completed 28.01.21

--- Chapter_6/Part_2/find_digit_in_number_Copy.cpp
+++ Chapter_6/Part_2/find_digit_in_number_Copy.cpp
@@ -3,13 +3,11 @@
 using namespace std;
 char NumberString[10];
 char DigitAt(int digit){
-    char found_digit;
     for (int i = 1; i < strlen(NumberString); ++i) {
         NumberString[i-1]=NumberString[i];
     }
-    found_digit=NumberString[0];
     if (digit==2){
-        return found_digit;
+        return NumberString[0];
     } else {
         DigitAt(--digit);
     }
