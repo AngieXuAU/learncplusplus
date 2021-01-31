cf63d77c5307478f767063167a0217fa8ba9ed68
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-02-01 07:34:46 +1100

    Random other practice skills test - morning 01.02

---
+++ Other/input_to_k.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+int Input[100], SumOfDigits=0;
+using namespace std;
+int DigitSum(int a){
+    SumOfDigits=0;
+    if (a==0){
+        return SumOfDigits;
+    } else if(a<10){
+        return a;
+    } else {
+        SumOfDigits+=a%10;
+        a/=10;
+    }
+}
+int main(){
+    int K, Index=0, Product=1;
+    cin>>K;
+    do {
+        cin>>Input[Index];
+    } while (Input[Index++]!=0);
+    for (int j = 0; j < Index; ++j) {
+        if (Input[j]<K*K && Input[j]!=0){
+            Product*=Input[j];
+        }
+    }
+    cout<<Product<<endl;
+    for (int j = 0; j < Index; ++j) {
+        if (Input[j]>=667 && Input[j]<=999){
+            cout<<Input[j]<<"\t";
+        }
+    }
+    cout<<endl;
+    for (int j = 0; j < Index; ++j) {
+        if (DigitSum(Input[j])%7==0 && Input[j]!=0){
+            cout<<Input[j]<<'\t';
+        }
+    }
+    cout<<endl;
+    return 0;
+}
---
+++ Other/one_to_two_hundred.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void Output(int number1, int number2){
+    int Index=4;
+    if (number2>101){
+        while (Index>=1) {
+            cout<<++number1<<"\t";
+            Index--;
+        }
+        Index=4;
+        while (Index>=1){
+            cout<<--number2<<" ";
+            Index--;
+        }
+        Output(number1, number2);
+    } else {
+        return;
+    }
+}
+int main(){
+    int Number1=0, Number2=200;
+    Output(Number1, Number2);
+    return 0;
+}
