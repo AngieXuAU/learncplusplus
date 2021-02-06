ed4f97480b61d73f0c4352e84521ff561d49223d
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-02-06 22:02:08 +1100

    Random other practice skills 06.02

--- Other/input_to_k.cpp
+++ Other/input_to_k.cpp
@@ -2,14 +2,12 @@
 int Input[100], SumOfDigits=0;
 using namespace std;
 int DigitSum(int a){
-    SumOfDigits=0;
-    if (a==0){
-        return SumOfDigits;
-    } else if(a<10){
-        return a;
+    if(a<10){
+        return SumOfDigits+=a;
     } else {
         SumOfDigits+=a%10;
         a/=10;
+        DigitSum(a);
     }
 }
 int main(){
@@ -18,24 +16,24 @@ int main(){
     do {
         cin>>Input[Index];
     } while (Input[Index++]!=0);
-//    for (int j = 0; j < Index; ++j) {
-//        if (Input[j]<K*K && Input[j]!=0){
-//            Product*=Input[j];
-//        }
-//    }
-//    cout<<Product<<endl;
-
-    for (int j = 0; j < Index-1; ++j) {
+    for (int j = 0; j < Index; ++j) {       //function for printing the numbers less than k squared
+        if (Input[j]<K*K && Input[j]!=0){
+            Product*=Input[j];
+        }
+    }
+    cout<<Product<<endl;
+    for (int j = 0; j < Index-1; ++j) {     //function for printing the numbers from 667-999
         if (Input[j]>=667 && Input[j]<=999){
             cout<<Input[j]<<"\t";
         }
     }
-//    cout<<endl;
-//    for (int j = 0; j < Index; ++j) {
-//        if (DigitSum(Input[j])%7==0 && Input[j]!=0){
-//            cout<<Input[j]<<'\t';
-//        }
-//    }
-//    cout<<endl;
+    cout<<endl;
+    for (int j = 0; j < Index; ++j) {       //function for printing the numbers whose digit sum a multiple of 7
+        SumOfDigits=0;
+        if (DigitSum(Input[j])%7==0 && Input[j]!=0){
+            cout<<Input[j]<<'\t';
+        }
+    }
+    cout<<endl;
     return 0;
 }
