94497d27f368c34da8dc6626546145227dfa12c3
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-02-08 22:29:49 +1100

    Extra struct practice 08.02

--- Chapter_7/Part_2/Extra_struct_practice/details_of_bank_users.cpp
+++ Chapter_7/Part_2/Extra_struct_practice/details_of_bank_users.cpp
@@ -1,33 +1,28 @@
 #include <cstdio>
-struct BankAccount{
+struct BankAccount{                     //struct setup
     char Name[20];
     int AccountNumber;
     int Balance;
 };
 BankAccount Member[5];
-int Add100(int balance){
-    if (balance>1000){
-        return balance+100;
-    } else {
-        return balance;
-    }
-}
 int main(){
     int i;
-    for (i = 0; i < 5; ++i) {
+    for (i = 0; i < 5; ++i) {           //input
         printf("Enter details of user %d: \n", i+1);
-        Member[i].AccountNumber=i+1;
+        Member[i].AccountNumber=i;
         scanf("%s %d", &Member[i].Name, &Member[i].Balance);
     }
-    for (int j = 0; j < i; ++j) {
+
+    for (int j = 0; j < i; ++j) {       //details of users whose balance is less than 200
         if (Member[j].Balance<200){
-            printf("%s \n", Member[i].Name);
+            printf("%s's account balance is less than $200\n", Member[j].Name);
         }
     }
-//    for (int j = 0; j < i; ++j) {
-//        if (Member[j].Balance>1000){
-//            printf("%s's account balance is %d \n", Member[i].Name, Add100(Member[j].Balance));
-//        }
-//    } 's account balance is less than $200",
+
+    for (int j = 0; j < i; ++j) {       //details of users whose balance is more than 1000
+        if (Member[j].Balance>1000){
+            printf("%s's account balance is $%d \n", Member[j].Name, Member[j].Balance+100);
+        }
+    }
     return 0;
 }
