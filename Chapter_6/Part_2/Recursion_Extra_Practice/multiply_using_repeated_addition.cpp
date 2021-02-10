532d45ebdf6ea5f5b4c74b67fbf9f4419cf70813
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-02-10 21:03:55 +1100

    Back to recursion 10.02

--- Chapter_6/Part_2/Recursion_Extra_Practice/multiply_using_repeated_addition.cpp
+++ Chapter_6/Part_2/Recursion_Extra_Practice/multiply_using_repeated_addition.cpp
@@ -1,16 +1,16 @@
 #include <cstdio>
-int Multiply(int a, int b){
-    int Product=1;
+int Product=0;
+int Multiply(int a, int b){;
     if(b==0){
         return Product;
     } else {
-        Product*=a;
-        Multiply(a, b--);
+        Product+=a;
+        Multiply(a, (--b));
     }
 }
 int main(){
     int A, B;
     scanf("%d %d", &A, &B);
-    printf("The product of %d and %d is %d", A, B, Multiply(A, B));
+    printf("%d",Multiply(A, B));
     return 0;
 }
