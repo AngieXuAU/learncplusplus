355a5c3f0a389202c5aaeb0aa2c3ab4b1ae2644f
AngieXuAU <angelinaxuwenlan@gmail.com>
2021-03-16 07:20:09 +1100

    Testing pointers

--- Chapter_8/Introductory/pointer_testing_ideas.cpp
+++ Chapter_8/Introductory/pointer_testing_ideas.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 const int Control=5;
 int main(){
-    int *PointerArray[Control], IntArray[Control];
-    for (int i = 0; i < Control; ++i) {
-        cin>>IntArray[i];
-        PointerArray[i]=&IntArray[i];
-        cout<<*PointerArray[i]<<endl;
-    }
+//    int *PointerArray[Control], IntArray[Control];
+//    for (int i = 0; i < Control; ++i) {
+//        cin>>IntArray[i];
+//        PointerArray[i]=&IntArray[i];
+//        cout<<*PointerArray[i]<<endl;
+//    }
+//    int a=88, *p=&a;
+//    cout<<a<<" "<<p<<" "<<&a;
+    int i=10, j=20;
+    int *p=&i, *s=&j;
+    cout<<j;
 }
